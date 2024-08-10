1c8c1a2f11b2c044526e0c8512bffcdf6d9c73ab
giuliano-ferrari <giuliano.ferrari@calendly.com>
2024-08-10 20:23:04 -0300

    local changes

--- alias.sh
+++ alias.sh
@@ -1,5 +1,6 @@
 ## Exa
-alias l="exa  -l --icons --color=always --header --git --created --changed --no-user --no-permissions --sort modified --all --binary"
+alias l="eza  -l --icons --color=always --header --git --created --changed --no-user --no-permissions --sort modified --all --binary"
 
 alias vim="nvim"
 alias v="vim"
+alias cd="z"
