18202ef486fb21864645e4dfc03c8dc52a414f15
Giuco <giucoferrari@gmail.com>
2025-11-24 20:37:12 -0300

    Add Zed editor configuration files with symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,5 @@
 ln -s -f ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -s -f ~/dotfiles/nvim ~/.config/nvim
 ln -s -f ~/dotfiles/karabiner ~/.config/karabiner
+ln -s -f ~/dotfiles/keymap.json ~/.config/zed/keymap.json
+ln -s -f ~/dotfiles/settings.json ~/.config/zed/settings.json
