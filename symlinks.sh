859b13d540aa506bb82f2100a62f463cf0b49139
giuliano-ferrari <giuliano.ferrari@calendly.com>
2025-11-24 20:41:57 -0300

    Organize Zed config files into dedicated folder

--- symlinks.sh
+++ symlinks.sh
@@ -1,5 +1,5 @@
 ln -s -f ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -s -f ~/dotfiles/nvim ~/.config/nvim
 ln -s -f ~/dotfiles/karabiner ~/.config/karabiner
-ln -s -f ~/dotfiles/keymap.json ~/.config/zed/keymap.json
-ln -s -f ~/dotfiles/settings.json ~/.config/zed/settings.json
+ln -s -f ~/dotfiles/zed/keymap.json ~/.config/zed/keymap.json
+ln -s -f ~/dotfiles/zed/settings.json ~/.config/zed/settings.json
